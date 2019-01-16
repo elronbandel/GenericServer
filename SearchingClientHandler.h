c0ad1f1d4e0e4339347f31f69a0193ce03dabeed
Fatuity <nxhvnkl123@gmail.com>
2019-01-16 15:39:24 +0200

    SearchingClientHandler is now complete, but still need to test it.

--- SearchingClientHandler.h
+++ SearchingClientHandler.h
@@ -10,19 +10,60 @@
 #include "Solver.h"
 #include "MatrixCreator.h"
 #include "MatrixGraph.h"
+#include "CacheManager.h"
 
 template<class Problem, class Solution>
 class SearchingClientHandler : public ClientHandler {
-    Solver<Problem, Solution> solver;
-//    CacheManager cacheManager;
+    Solver<Problem, Solution> *solver;
+    CacheManager<string, string> *cacheManager;
+
+    pair<int, int> getRowAndCol(string type) {
+        string str;
+        str += type[0];
+        int row = stoi(str);
+        str = "";
+        str += type[type.size() - 1];
+        int col = stoi(str);
+        pair<int, int> rowAndCol(row, col);
+        return rowAndCol;
+    }
+
+    string getPath(Solution solution) {
+        size_t size = solution.size();
+        string str, path;
+        pair<int, int> currState;
+        pair<int, int> nextState;
+        if (!solution.empty()) {
+            for (int i = 1; i < size; i++) {
+                currState = getRowAndCol((string) solution.at(i - 1));
+                nextState = getRowAndCol((string) solution.at(i));
+                if (currState.first < nextState.first) {
+                    path += "Down";
+                } else if (currState.first > nextState.first) {
+                    path += "Up";
+                } else if (currState.second < nextState.second) {
+                    path += "Right";
+                } else if (currState.second > nextState.second) {
+                    path += "Left";
+                }
+                if (i != size - 1) {
+                    path += ",";
+                }
+            }
+        }
+        return path;
+    }
+
 public:
-//    SearchingClientHandler(Solver<Problem,Solution> s, CacheManager cm){
-//        solver = s;
-//        cacheManager = cm;
-//    }
+    SearchingClientHandler(Solver<Problem, Solution> *s, CacheManager<string, string> *cm) {
+        solver = s;
+        cacheManager = cm;
+    }
+
     virtual void handle(istream &sin, ostream &sout) {
         string line, result, matrixRow, helpStr, initStr, goalStr;
         vector<string> rows;
+        string matrixString;
         unsigned long int endOfRow;
         while (line != "end") {
             //sin will override the info if there is info left in the line.
@@ -32,13 +73,20 @@ public:
                 sin >> helpStr;
                 line += helpStr;
             }
+            //concatenate the lines received from stream;
             //notice that the line might contain another row.
-            while (endOfRow = line.find("\n") != -1) {
+            while ((endOfRow = line.find("\n")) != -1) {
                 //if we got a few rows we need to get each of them separate.
                 matrixRow = line.substr(0, endOfRow);
                 rows.push_back(matrixRow);
-                line = line.substr(endOfRow, line.size() - 1);
+                matrixString += matrixRow + "$";
+                line = line.substr(endOfRow + 1, line.size() - 1);
             }
+        }
+        //check if the solution exists, if it does, return it to the client.
+        if (cacheManager->contains(matrixString)) {
+            sout << cacheManager->request(matrixString);
+            return;
         }
         //we finished receiving information, check if "end" entered the vector and remove it.
         if (rows.back() == "end") {
@@ -49,16 +97,15 @@ public:
         rows.pop_back();
         initStr = rows.back();
         rows.pop_back();
-        //solve the problem;
-        MatrixCreator<string> matrixCreator;
-        MatrixGraph<string> mg = matrixCreator.create(rows, initStr, goalStr);
-        Solution *solution = solver.solve(mg);
-        size_t size = solution->size();
-        for (int i = 0; i < size; i++) {
-            result += solution->at(i);
-        }
+
+        //create and solve the problem.
+        Searchable<string> *mg = MatrixCreator<string>::create(rows, initStr, goalStr);
+        Solution solution = solver->solve(mg);
+        //get the wanted result to the client and save it in the cache.
+        result = this->getPath(solution);
         sout << result;
         sout.flush();
+        cacheManager->store(matrixString, result);
     }
 };
 

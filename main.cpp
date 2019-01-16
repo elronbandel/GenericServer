f02095fd47151ec9b2ffa97fd3c65515818e276c
Fatuity <nxhvnkl123@gmail.com>
2019-01-16 23:40:37 +0200

    main

--- main.cpp
+++ main.cpp
@@ -10,6 +10,7 @@
 #include "DFS.h"
 #include "BestFirstSearch.h"
 #include "ParallelServer.h"
+#include "AStar.h"
 #include "SearchingClientHandler.h"
 #include "FileCacheManager.h"
 #include <ctime>
@@ -28,7 +29,8 @@ int main(int argc, char* argv[]) {
         vector<State<string> *> path;
         string row;
         vector<string> rows;
-        ISearcher<vector<State<string> *>, string> *searcher = new DFS<vector<State<string> *>, string>();
+        EuklidianHuresticator huresticator;
+        ISearcher<vector<State<string> *>, string> *searcher = new AStar<vector<State<string> *>, string,EuklidianHuresticator>(huresticator);
         Solver<Searchable<string>, vector<State<string> *>>* solver =
                  new SearchSolver<Searchable<string>, vector<State<string> *>, string>(searcher);
         CacheManager<string,string>* cacheManager = new FileCacheManager<string,string>("cache.txt");
